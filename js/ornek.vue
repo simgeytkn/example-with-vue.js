a23be28106e3692dffdb1513db6cde9e092c7a1a
simgeytkn <36228273+simgeytkn@users.noreply.github.com>
2018-06-18 03:41:03 +0300

    Add files via upload

---
+++ js/ornek.vue
@@ -0,0 +1,33 @@
+new Vue({
+    el: "#app",
+    data: {
+        result:""        
+    },
+    methods: {
+        add: function(){
+            number1=+document.getElementById("number1").value,
+            number2=+document.getElementById("number2").value,
+            this.result=number1+number2
+        },
+
+        sub: function(){
+
+            number1=+document.getElementById("number1").value,
+            number2=+document.getElementById("number2").value,
+            this.result=number1-number2
+
+        },
+
+        div: function(){
+            number1=+document.getElementById("number1").value,
+            number2=+document.getElementById("number2").value,
+            this.result = parseFloat(number1/number2)
+        }, 
+
+        mul: function(){
+            number1=+document.getElementById("number1").value,
+            number2=+document.getElementById("number2").value,
+            this.result = number1*number2
+        }
+    }
+})
